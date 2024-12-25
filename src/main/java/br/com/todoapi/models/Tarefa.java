8ec207a007b91c84c3feb23339f31d7bdfbeb03e
million212 <henbarros694@gmail.com>
2024-12-25 20:38:38 -0300

    Finalização da classe Tarefa.java

--- src/main/java/br/com/todoapi/models/Tarefa.java
+++ src/main/java/br/com/todoapi/models/Tarefa.java
@@ -1,4 +1,31 @@
 package main.java.br.com.todoapi.models;
 
 public class Tarefa {
+    private int id;
+    private String titulo;
+    private String descricao;
+    private String status; // "PENDENTE", "EM PROGRESSO", "CONCLUIDA"
+
+    // Construtor
+
+    public Tarefa(int id, String titulo, String descricao, String status) {
+        this.id = id;
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.status = status;
+    }
+
+    // Getters e Setters
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getTitulo() { return titulo; }
+    public void setTitulo(String titulo) { this.titulo = titulo; }
+
+    public String getDescricao() { return descricao; }
+    public void setDescricao(String descricao) { this.descricao = descricao; }
+
+    public String getStatus() { return status; }
+    public void setStatus(String status) { this.status = status; }
 }
