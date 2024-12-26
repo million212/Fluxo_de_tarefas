3e9e7430a5eede000c01fdcdaa8a3c1097605547
million212 <henbarros694@gmail.com>
2024-12-25 22:51:32 -0300

    Finalização da classe TarefaRepository.java

--- src/main/java/br/com/todoapi/repositories/TarefaRepository.java
+++ src/main/java/br/com/todoapi/repositories/TarefaRepository.java
@@ -1,4 +1,28 @@
 package main.java.br.com.todoapi.repositories;
 
+import main.java.br.com.todoapi.models.Tarefa;
+import java.util.ArrayList;
+import java.util.List;
+
 public class TarefaRepository {
+    private List<Tarefa> tarefas = new ArrayList<>();
+    private int idCounter = 1;
+
+    public List<Tarefa> findAll() {
+        return tarefas;
+    }
+
+    public Tarefa findById(int id) {
+        return tarefas.stream().filter(t -> t.getId() == id).findFirst().orElse(null);
+    }
+
+    public Tarefa save(Tarefa tarefa) {
+        tarefa.setId(idCounter++);
+        tarefas.add(tarefa);
+        return tarefa;
+    }
+
+    public boolean delete(int id) {
+        return tarefas.removeIf(t -> t.getId() == id);
+    }
 }
